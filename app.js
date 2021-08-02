5d56c7f06b670005a16d6da156552903215a3ebc
L410N3L <57776908+L410N3L@users.noreply.github.com>
2021-08-01 22:44:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      name: 'Bitcoin',
+      img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 0,
+      prices: [100, 200, 300 ,10000, 90909],
+
+      pricesWithDays: [
+        { day: 'Lunes', value: 8400 },
+        { day: 'Martes', value: 7900 },
+        { day: 'Miercoles', value: 8200 },
+        { day: 'Jueves', value: 9000 },
+        { day: 'Viernes', value: 9400 },
+        { day: 'Sabado', value: 10000 },
+        { day: 'Domingo', value: 10200 },
+    ]
+    }
+  }
+})
